b439c8d41ba92435a8255baf5bc7377d5fb2a1a0
vtch99 <crystal.tyan@gmail.com>
2015-03-24 10:16:53 +0800

    update

---
+++ APP/www/js/views/profile.js
@@ -0,0 +1,24 @@
+
+AeroMap.Views = AeroMap.Views || {}
+
+AeroMap.Views.ProfileView = Backbone.View.extend({
+    el: '.am-context',
+    
+    template: JST['js/template/profile.ejs'],
+    
+    initialize: function () {
+        
+    },
+   
+    render: function () {
+        
+    },
+    
+    show: function () {
+        
+    },
+
+    clean: function () {
+        
+    }
+})
