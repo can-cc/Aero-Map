db11a1fd9f82d9cc6f6fb7c90a38a18249bec146
vtch99 <crystal.tyan@gmail.com>
2015-03-25 10:29:13 +0800

    update

--- APP/www/js/main.js
+++ APP/www/js/main.js
@@ -6,13 +6,25 @@ $(function() {
     FastClick.attach(document.body)
 })
 
+$(document).foundation({
+  offcanvas : {
+    // Sets method in which offcanvas opens.
+    // [ move | overlap_single | overlap ]
+    open_method: 'move', 
+    // Should the menu close when a menu link is clicked?
+    // [ true | false ]
+    close_on_click : true
+  }
+});
+
 window.AeroMap = {
     Models: {},
     Collections: {},
     Views: {},
     Routers: {},
     Options: {
-        Map_Max: 13,
+        Map_Min: 13,
+        Map_Default: 14,
         Debug: true
     },
     init: function() {
--- APP/www/js/views/map.js
+++ APP/www/js/views/map.js
@@ -12,39 +12,46 @@ AeroMap.Views.MapView = Backbone.View.extend({
 
     map: null,
 
-    initialize: function() {
+    initialize: function () {
         L.mapbox.accessToken = 'pk.eyJ1IjoidnRjaDk5IiwiYSI6IlRFVlFoTkEifQ.YD7Rx4C15GrS25eAJcuZtQ'
         this.render()
     },
 
-    render: function() {
+    render: function () {
         this.map = L.mapbox.map(this.el, 'vtch99.lea375p3')
     },
 
-    show: function() {
+    show: function () {
         $(this.other_el).hide()
         this.$el.show()
+        this.location(this.position)
     },
 
-    foot_record: function(position) {
-        
+    foot_record: function (position) {
+
     },
 
-    location: function(position) {
+    location: function (map_location) {
+        var b_move_center = this.move_center.bind(this)
+        var b_location_error = this.location_error.bind(this)
         navigator.geolocation.getCurrentPosition(function(position) {
-            this.position = position
-            this.move_center(position)
-            this.foot_record(position)
-            },
-            this.get_position_error, {
+            map_location = position
+            b_move_center(position)
+        }, b_location_error, {
                 enableHighAccuracy: true
             })
     },
 
     move_center: function (position) {
+        console.log('fu')
         var latitude = position.coords.latitude
+        console.log(latitude)
         var longitude = position.coords.longitude
-        this.map.setView([40, -74.50], 9)
+        this.map.setView([latitude, longitude],  AeroMap.Options.Map_Default)
+    },
+
+    location_error: function () {
+        
     }
 
 })
