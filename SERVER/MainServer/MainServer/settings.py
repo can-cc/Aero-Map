c98a0ee98a977477609e9d1a982331fe06bbfb02
vtch99 <crystal.tyan@gmail.com>
2015-03-30 07:44:14 +0000

    use Django

--- SERVER/MainServer/MainServer/settings.py
+++ SERVER/MainServer/MainServer/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
