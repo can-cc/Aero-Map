e3bc30b273f9cc04d4f4195adc1cab0a179621f0
AbyChan <crystal.tyan@gmail.com>
2015-04-05 15:23:34 +0000

    update

---
+++ SERVER/ReserveServer/database/schema/user.js
@@ -0,0 +1,155 @@
+var knex = require('../../db').knex,
+    async = require('async'),
+    winston = require('winston')
+
+exports.createUsers = function() {
+    return knex.schema.createTable('Users', function(table) {
+        table.increments().unique()
+        table.string('username')
+            .unique()
+            .notNullable()
+            .index()
+        table.string('password')
+            .notNullable()
+        table.string('email')
+            .unique()
+            .notNullable()
+        table.timestamps()
+    })
+}
+
+exports.createUsersDetail = function() {
+    return knex.schema.createTable('UsersDetail', function(table) {
+        table.increments().unique()
+        table.integer('user_id')
+            .unique()
+            .unsigned()
+            .notNullable()
+            .index()
+            .references('id')
+            .inTable('Users')
+        table.string('nickname')
+            .unique()
+            .notNullable()
+        table.enu('sex', ['male', 'female', 'secert'])
+            .notNullable()
+        table.text('self_description')
+        table.integer('city')
+        table.integer('school')
+        table.integer('interest')
+        table.string('public_mail')
+        table.string('phono_number')
+        table.string('qq')
+    })
+}
+
+
+exports.createUsersSetting = function() {
+
+}
+
+exports.createUsersInfomation = function() {
+
+}
+
+exports.drop_all_tables = function(callback) {
+    async.series([
+
+        function(next) {
+            knex.schema.dropTableIfExists('UsersDetail').then(function(success) {
+                winston.info('create UsersDetail table', JSON.stringify(success))
+                next(null, true)
+            }, function(error) {
+                winston.error('drop UsersDetail table', JSON.stringify(error))
+                next(new Error('drop UserDetail error'), false)
+            })
+        },
+        function(next) {
+            knex.schema.dropTableIfExists('Users').then(function(success) {
+                winston.info('drop Users table', JSON.stringify(success))
+                next(null, true)
+            }, function(error) {
+                winston.error('drop Users table', JSON.stringify(error))
+                next(new Error(), false)
+            })
+        }
+
+    ], function(err, result) {
+        winston.info('error', err)
+        if (err) {
+            return callback(err)
+        }
+        winston.info(result)
+        return callback(null, result.every(function(flag) {
+            return flag
+        }))
+    })
+}
+
+exports.create_all_tables = function(callback) {
+    async.series([
+        function(call) {
+            exports.createUsers().then(
+                function(success) {
+                    winston.log('info', 'create Users table success', success)
+                    call(null, true)
+                },
+                function(error) {
+                    winston.info('error', 'create Users table', JSON.stringify(error))
+                    call(new Error('create Users table error'), false)
+                })
+        },
+        function(call) {
+            exports.createUsersDetail().then(
+                function(success) {
+                    winston.log('info', 'create UsersDetail table success', success)
+                    call(null, true)
+                },
+                function(error) {
+                    winston.log('error', 'create UsersDetail table error', JSON.stringify(error))
+                    call(new Error('create Users table error'), false)
+                })
+        }
+    ], function(err, results) {
+        winston.log('debug', err)
+        winston.log('debug', results)
+        if (err) return callback(err)
+        return callback(null, results.every(function(flag) {
+            return flag
+        }))
+    })
+}
+
+exports.truncate_all_tables = function(callback) {
+    async.series([
+        function(call){
+            exports.drop_all_tables(function(err, result){
+                if(err) return call(err)
+                if(result){
+                    return call(null, true)
+                } else {
+                    return call(new Error('Drop All Tables Error!'))
+                }
+            })
+        },
+        function(call){
+            exports.create_all_tables(function(err, result){
+                if(err) return call(err)
+                if(result){
+                    return call(null, true)
+                } else {
+                    return call(new Error('Create All Tables Error!'))
+                }
+            })
+        }
+    ], function(err, results){
+        winston.log('debug', err)
+        winston.log('debug', results)
+        if (err) return callback(err)
+        return callback(null, results.every(function(flag) {
+            return flag
+        }))
+    })
+}
+
+
---
+++ SERVER/ReserveServer/test/database/user_schema.js
@@ -0,0 +1,97 @@
+var assert = require('assert'),
+    async = require('async'),
+    user_schema = require('../../database/schema/user')
+
+
+
+describe('operation', function() {
+
+
+
+
+
+    describe('#create User table', function() {
+        it('should return promise and success', function(done) {
+            user_schema.createUsers().then(
+                function() {
+                    done()
+                },
+                function(err) {
+                    throw err
+                }
+            )
+        })
+    })
+
+
+
+    describe('#create UsersDetail table', function() {
+        it('should return promise and success', function(done) {
+            user_schema.createUsersDetail().then(
+                function() {
+                    done()
+                },
+                function(err) {
+                    throw err
+                })
+        })
+    })
+
+
+})
+
+
+
+describe('all_operation', function() {
+    describe('#drop_all_table', function() {
+        it('shold return true', function(done) {
+            user_schema.drop_all_tables(function(err, result) {
+                if (err) throw err
+                if (result) {
+                    done()
+                } else {
+                    throw new Error()
+                }
+            })
+        })
+    })
+
+    describe('#create_all_table', function() {
+        it('should return true', function(done) {
+            user_schema.create_all_tables(function(err, result) {
+                if (err) throw err
+                if (result) {
+                    done()
+                } else {
+                    throw new Error()
+                }
+            })
+        })
+    })
+
+    describe('#truncate_all_table', function() {
+        it('should return true', function(done) {
+            user_schema.truncate_all_tables(function(err, result) {
+                if (err) throw err
+                if (result) {
+                    done()
+                } else {
+                    throw new Error()
+                }
+            })
+        })
+    })
+
+    describe('#drop_all_table', function() {
+        it('shold return true', function(done) {
+            user_schema.drop_all_tables(function(err, result) {
+                if (err) throw err
+                if (result) {
+                    done()
+                } else {
+                    throw new Error()
+                }
+            })
+        })
+    })
+})
