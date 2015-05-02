9f4342769002daa79c2d42c1de86109e745cea04
AbyChan <crystal.tyan@gmail.com>
2015-05-02 16:28:03 +0800

    update

--- APP-Angular/www/js/app.js
+++ APP-Angular/www/js/app.js
@@ -3,6 +3,7 @@ angular.module('Aero', [
         'leaflet-directive',
         'Aero.controllers',
         'mm.foundation',
+  'ngImgCrop',
         'Aero.config'
     ])
     .config(function($httpProvider, $stateProvider, $urlRouterProvider) {
@@ -24,16 +25,26 @@ angular.module('Aero', [
                 templateUrl: 'templates/register.html',
                 controller: 'RegCtrl'
             })
+            .state('detail', {
+                url: '/detail',
+                templateUrl: 'templates/user_detail.html',
+                controller: 'DetailCtrl'
+            })
+        .state('avatar', {
+          url: '/avatar',
+          templateUrl: 'templates/avatar.html',
+          controller: 'AvatarCtrl'
+        })
             .state('map', {
                 url: '/map',
                 templateUrl: 'templates/map.html',
                 controller: 'MapCtrl'
             })
-        .state('post', {
-          url: '/post',
-          templateUrl: 'templates/post.html',
-          controller: 'PostCtrl'
-        });
+            .state('post', {
+                url: '/post',
+                templateUrl: 'templates/post.html',
+                controller: 'PostCtrl'
+            });
 
 
     });
